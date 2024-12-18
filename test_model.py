f44bc0d8d0d9b1fec868cdc667d61c5a755cb1fb
giannisak <154079765+giannisak@users.noreply.github.com>
2024-12-18 08:16:58 +0200

    Create test_model.py

---
+++ test_model.py
@@ -0,0 +1,13 @@
+# Test NER model behavior on a single example
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+example = "Alvin Lee"
+
+
+ner_results = nlp(example)
+print(ner_results)
